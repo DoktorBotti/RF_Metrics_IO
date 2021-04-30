a6cd4b5883d36c7e6ec09fabf34ff52bd24cc200
Tobias Bodmer <Tobias.Bodmer@gmx.de>
2021-04-30 17:00:21 +0200

    reformat

--- IOData.cpp
+++ IOData.cpp
@@ -4,14 +4,14 @@
 
 #include "IOData.h"
 bool io::IOData::operator==(const io::IOData &rhs) const {
-  return mean_rf_dst == rhs.mean_rf_dst &&
-         split_score_calc == rhs.split_score_calc &&
-         meanModifiedRFDst == rhs.meanModifiedRFDst &&
-         taxa_names == rhs.taxa_names &&
-         pairwise_distance_mtx == rhs.pairwise_distance_mtx &&
-         git_revision == rhs.git_revision &&
-         cpuInformation == rhs.cpuInformation;
+    return mean_rf_dst == rhs.mean_rf_dst &&
+           split_score_calc == rhs.split_score_calc &&
+           meanModifiedRFDst == rhs.meanModifiedRFDst &&
+           taxa_names == rhs.taxa_names &&
+           pairwise_distance_mtx == rhs.pairwise_distance_mtx &&
+           git_revision == rhs.git_revision &&
+           cpuInformation == rhs.cpuInformation;
 }
 bool io::IOData::operator!=(const io::IOData &rhs) const {
-  return !(rhs == *this);
+    return !(rhs == *this);
 }
--- IOData.h
+++ IOData.h
@@ -6,19 +6,19 @@
 
 namespace io {
 struct IOData {
-  double mean_rf_dst = 0;
-  enum Metric { MCI, MSI, SPI } split_score_calc;
-  double                   meanModifiedRFDst = 0;
-  std::vector<std::string> taxa_names;
-  // Invariant: numEntries = numTaxa - rowID
-  std::vector<std::vector<double>> pairwise_distance_mtx;
-  std::string                      git_revision;
-  std::string                      cpuInformation;
+    double mean_rf_dst = 0;
+    enum Metric { MCI, MSI, SPI } split_score_calc;
+    double                   meanModifiedRFDst = 0;
+    std::vector<std::string> taxa_names;
+    // Invariant: numEntries = numTaxa - rowID
+    std::vector<std::vector<double>> pairwise_distance_mtx;
+    std::string                      git_revision;
+    std::string                      cpuInformation;
 
-private:
-public:
-  bool operator==(const IOData &rhs) const;
-  bool operator!=(const IOData &rhs) const;
+  private:
+  public:
+    bool operator==(const IOData &rhs) const;
+    bool operator!=(const IOData &rhs) const;
 };
 NLOHMANN_JSON_SERIALIZE_ENUM(IOData::Metric,
                              {{IOData::MCI, "MCI"},
